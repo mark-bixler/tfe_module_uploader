77ab088447d182a71f7d321da7c8747e52b7948b
Mark Bixler <mark.bixler@mindbodyonline.com>
2019-01-21 17:20:43 -0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# PTFE Module Updater
+
+  Simple bash script utilizing the PTFE (Private Terraform Enterpise) API to create, version and upload Terraform modules to the PTFE Private Module Registry. 
+
+  - The script uses the current module folder name as the name for the module to be uploaded. 
+  - A version number is required for the module. 
+    - If the module is new, the script automatically versions the module as "1.0.0"
+  - The script creates a temporary *.tar.gz file to upload the files to module registry and removes the archive on script exit.
+
+  ## Getting Started
+
+  ### Dependencies
+
+  - bash 3.2+
+  - PTFE User Token
+  - Using standard MindBody PTFE Instance
+    - "mindbody" as organization
+    - "mb" as Module Provider
+    - ** These values can be edited in the script as global varibles if needed **
+
+  ### Installing
+
+  - Clone / Download source code.
+  - Copy *moduleUploder.sh* into module directory
+
+  ### Executing program
+
+  - Set $TOKEN environment variable 
+
+    - `export TOKEN=aFoFQ4nLtgkA19g.atlasv1....`
+
+  - Run moduleUploader.sh
+    ​`
+    ./moduleUploader.sh​`
+
+  - You can also run the command with the **TOKEN** as an argument
+
+    `./moduleUploader.sh TOKEN=aFoFQ4nLtgkA19g.atlasv1....`
+
+  ## Help
+
+  `moduleUploader.sh -h`
+
+  ## Authors
+
+  Mark Bixler  
+  mark.bixler@mindbodyonline.com
+
+  ## Version History
+
+  - 0.1
+    - Initial Release
---
+++ moduleUploader.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+# https://www.terraform.io/docs/enterprise/api/modules.html
+
+################################################################################
+### Set Variables ###
+################################################################################
+url=https://ptfe.tools.mindbody.io
+name=${PWD##*/}
+org="mindbody"
+provider="mb"
+
+################################################################################
+### Help ###
+################################################################################
+if [ "$1" == "-h" ]; then
+  echo
+  echo "Usage: ./`basename $0` [TOKEN]"
+  echo
+  echo "eg. ./`basename $0` TOKEN=aFoFQ4nLtgkA19g.atl...."
+  echo
+  exit 0
+fi
+
+################################################################################
+### New or existing module ###
+################################################################################
+read -p "New or Existing Module? [New]: " mod
+mod=${mod:-New}
+if [[ $(tr "[:upper:]" "[:lower:]" <<<$mod) -ne "existing" ]] || \
+    [[ $(tr "[:upper:]" "[:lower:]" <<<$mod) -ne "new" ]];
+then
+  echo "Invalid response. Quitting.."
+  exit
+fi
+################################################################################
+create_new_module()
+{
+  cat <<EOF
+{
+  "data": {
+    "type": "registry-modules",
+    "attributes": {
+      "name": "$name",
+      "provider": "$provider"
+    }
+  }
+}
+EOF
+}
+################################################################################
+create_module_version()
+{
+  cat <<EOF
+{
+  "data": {
+    "type": "registry-module-versions",
+    "attributes": {
+      "version": "$version"
+    }
+  }
+}
+EOF
+}
+################################################################################
+### Create Module ###
+################################################################################
+ if [[ $(tr "[:upper:]" "[:lower:]" <<<$mod) = "new" ]];
+ then
+  curl \
+    --header "Authorization: Bearer $TOKEN" \
+    --header "Content-Type: application/vnd.api+json" \
+    --request POST \
+    --data "$(create_new_module)" \
+    $url/api/v2/organizations/$org/registry-modules 
+
+  version="1.0.0" 
+else
+  echo -n "Enter module version number [eg. 1.0.0]: "
+  read version
+fi
+
+################################################################################
+### Create Module Version
+################################################################################
+
+output=`curl \
+  --header "Authorization: Bearer $TOKEN" \
+  --header "Content-Type: application/vnd.api+json" \
+  --request POST \
+  --data "$(create_module_version)" \
+  $url/api/v2/registry-modules/$org/$name/$provider/versions`
+
+uploadUrl=$(echo $output | awk -F 'upload":"' '{print substr($2, 1, length($2)-4)}')
+
+################################################################################
+### Archive Module Directory
+################################################################################
+
+tar zcvf $name.$version.tar.gz *
+
+################################################################################
+### Upload Module
+################################################################################
+
+curl \
+  --header "Content-Type: application/octet-stream" \
+  --request PUT \
+  --data-binary @$name.$version.tar.gz \
+  $uploadUrl
+
+  rm $name.$version.tar.gz
